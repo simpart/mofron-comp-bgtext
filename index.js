818ef94e849c302a1074b282be1529e506097203
simpart <taria.ki4m@gmail.com>
2017-08-12 13:53:38 +0900

    first

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * @file   mofron-comp-bgtext/index.js
+ * @author simpart
+ */
+
+/**
+ * @class BgText
+ * @brief background text component for mofron
+ */
+mofron.comp.text.BgText = class extends mofron.comp.Text {
+    
+    constructor (prm_opt) {
+        try {
+            super();
+            this.name('BgText');
+            this.prmOpt(prm_opt);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * initialize vdom
+     * 
+     * @param prm : (string) text contents
+     */
+    initDomConts (prm) {
+        try {
+            super.initDomConts();
+            this.style({
+                'text-shadow' : '3px 3px 5px white'
+            });
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+}
+mofron.comp.text.bgtext = {};
+module.exports = mofron.comp.text.BgText;
